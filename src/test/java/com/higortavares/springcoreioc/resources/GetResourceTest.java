513d77a7b79f02eb02c616a24f2227628ee4ffb4
paulo.tavares <paulo.tavares@olxbr.com>
2023-04-20 21:08:57 -0300

    read file using resource api

--- src/test/java/com/higortavares/springcoreioc/resources/GetResourceTest.java
+++ src/test/java/com/higortavares/springcoreioc/resources/GetResourceTest.java
@@ -7,7 +7,6 @@ import java.nio.file.Files;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.Resource;
 
 public class GetResourceTest {
 
